9f1bf9da585755c06ef90893b2f0b40c1ce7c67e
Tony Okeke <tonykabilanokeke@gmail.com>
2024-07-29 23:17:17 -0400

    feat: add persistence, wakatime; update which-key

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,6 +41,8 @@ require('lazy').setup({
   require 'plugins/barbar',
   require 'plugins/distant',
   require 'plugins/llm',
+  require 'plugins/wakatime',
+  require 'plugins/persistence',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
---
+++ lua/plugins/wakatime.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    'wakatime/vim-wakatime',
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
