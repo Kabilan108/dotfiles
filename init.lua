5a81de686dd0dd9360b232b007a829bbb7a1677a
Tony Okeke <muaddib@lisan-al-gaib>
2024-08-06 09:30:40 -0400

    feat: use system clipboard

--- init.lua
+++ init.lua
@@ -92,6 +92,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Use system clipboard
+vim.opt.clipboard:append 'unnamedplus'
+
 -- [[ Setting options ]]
 require 'options'
 
