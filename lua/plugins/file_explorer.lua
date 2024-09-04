f61859fc561bce895affd84f61f7a3e796d2d6cb
Tony Okeke <muaddib@lisan-al-gaib>
2024-09-03 23:41:43 -0400

    modify files shown in sidebar

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -15,6 +15,20 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = false,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {},
+        never_show = {
+          '.git',
+          '.DS_Store',
+          'thumbs.db',
+          '__pycache__',
+          'node_modules',
+        },
+      },
       window = {
         width = 25,
         mappings = {
