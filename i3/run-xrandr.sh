9ed9cb7ebed2bfe893f1c3616a30bb4ab1958a15
Tony Okeke <muaddib@lisan-al-gaib>
2024-10-14 12:09:53 -0400

    allow monitor set up to be controlled with an env var

--- i3/run-xrandr.sh
+++ i3/run-xrandr.sh
@@ -1,11 +1,28 @@
-#!/bin/sh
+#! /bin/bash
 
-# TODO: use this to set the layout
-# TODO: make this accept flags, and if not show a rofi menu to select the layout
-XRANDR_LAYOUT="lisan-al-gaib"
+source $HOME/.bash_env
 
-# Get the number of connected monitors
-MONITOR_COUNT=$(xrandr --query | grep " connected" | wc -l)
-echo $MONITOR_COUNT > /tmp/monitor_count
+if [ -z $XRANDR_LAYOUT ]; then
+  echo "XRANDR_LAYOUT is not set";
+  exit 1;
+fi
 
-xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+# Check the number of connected monitors
+monitor_count=$(xrandr --listmonitors | grep -c "^ ")
+
+if [ $monitor_count -eq 1 ]; then
+  xrandr --auto
+else
+  case $XRANDR_LAYOUT in
+    "lisan-al-gaib")
+      xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+      ;;
+    "moberg-work")
+      # Add xrandr command for moberg-work layout
+      ;;
+    *)
+      echo "Error: Unexpected layout '$XRANDR_LAYOUT'"
+      exit 1
+      ;;
+  esac
+fi
