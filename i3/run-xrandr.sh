9e0d781f1e8cbf540dd673567d515ca45e1f9bac
Tony Okeke <muaddib@lisan-al-gaib>
2024-10-16 19:34:25 -0400

    add config for work

--- i3/run-xrandr.sh
+++ i3/run-xrandr.sh
@@ -8,7 +8,8 @@ if [ -z $XRANDR_LAYOUT ]; then
 fi
 
 # Check the number of connected monitors
-monitor_count=$(xrandr --listmonitors | grep -c "^ ")
+# monitor_count=$(xrandr --listmonitors | grep -c "^ ")
+monitor_count=$(xrandr | grep " connected" | wc -l)
 
 if [ $monitor_count -eq 1 ]; then
   xrandr --auto
@@ -17,8 +18,9 @@ else
     "lisan-al-gaib")
       xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
       ;;
-    "moberg-work")
+    "moberg-office")
       # Add xrandr command for moberg-work layout
+      xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x458 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal
       ;;
     *)
       echo "Error: Unexpected layout '$XRANDR_LAYOUT'"
