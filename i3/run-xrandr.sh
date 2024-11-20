2b9f04bd177a435fe0c25a9fc009711326095be3
Tony Okeke <muaddib@lisan-al-gaib>
2024-11-20 00:16:29 -0500

    feat: add mirror mode for monitors

--- i3/run-xrandr.sh
+++ i3/run-xrandr.sh
@@ -18,6 +18,9 @@ else
     "lisan-al-gaib")
       xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
       ;;
+    "lisan-al-gaib__mirror")
+      xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off
+      ;;
     "moberg-office")
       # Add xrandr command for moberg-work layout
       xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x458 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal
