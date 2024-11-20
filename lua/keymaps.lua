b7de5c9a0a16d9505c496c79b61b8f4957398b5e
Tony Okeke <muaddib@lisan-al-gaib>
2024-11-19 23:38:16 -0500

    feat: clean up map function
    refactor: var renaming, organization

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,25 +1,39 @@
 -- keymaps.lua
 
+---- VARS -----------------------------------------------------------------------------
 local api = vim.api
 local cmd = vim.cmd
-
 local ts = require("telescope.builtin")
 
-local map = function(keys, func, desc, opts, mode)
-  vim.keymap.set(mode or "n", keys, func, { desc = desc }, opts or {})
+---- FUNCS ----------------------------------------------------------------------------
+local function map(keys, func, desc, mode)
+  local opts = { noremap = true, silent = true, desc = desc }
+  vim.keymap.set(mode or "n", keys, func, opts)
 end
 
--- add replace keymap
-map("<A-r>", ":s/", "replace", {}, "v")
+local function echo(msg)
+  cmd('echo "' .. msg .. '"')
+  vim.defer_fn(function()
+      cmd('echon ""')
+  end, 3000)
+end
+
+---- KEYMAPS --------------------------------------------------------------------------
+-- global replace
+map("<A-r>", ":%s/", "replace", { "n", "v" })
 
 -- neo-tree
-map("\\", ":Neotree toggle<CR>", "[\\] toggle file explorer")
+map("\\", "<CMD>Neotree toggle<CR>", "[\\] toggle file explorer")
 
 -- clear search highlights
-map("<Esc>", ":nohlsearch<CR>")
+map("<Esc>", "<CMD>nohlsearch<CR>")
 
 -- telescope
-map("<leader>b", ts.buffers, "[b]uffers", { sort_mru = true })
+map(
+  "<leader>b",
+  function() ts.buffers({ sort_mru = true }) end,
+  "[b]uffers"
+)
 map("<leader>sf", ts.find_files, "[s]earch [f]iles")
 map("<leader>sg", ts.live_grep, "[s]earch [g]rep")
 map("<leader>sh", ts.help_tags, "[s]earch [h]elp tags")
@@ -28,34 +42,32 @@ map("<leader>sr", ts.resume, "[s]earch [r]esume")
 map("<leader>s.", ts.oldfiles, "[s]earch [.] recent files")
 
 -- folding
-map("zc", ":foldclose<CR>", "close fold under cursor")
-map("zo", ":foldopen<CR>", "open fold under cursor")
-map("za", ":foldclose<CR>", "toggle fold under cursor")
+map("zc", "<CMD>foldclose<CR>", "close fold under cursor")
+map("zo", "<CMD>foldopen<CR>", "open fold under cursor")
+map("za", "<CMD>foldclose<CR>", "toggle fold under cursor")
 map("<leader>z", "za", "toggle fold")
 map("<leader>Z", "zA", "toggle fold recursively")
 
 -- mini.sessions
-local load_session = function ()
-  require('mini.sessions').read()
-end
-map("<leader>ls", load_session, "[l]oad [s]ession")
-map("<leader>ss", ":mksession<CR>", "[s]ave [s]ession")
+map(
+  "<leader>ls",
+  function () require('mini.sessions').read() end,
+  "[l]oad [s]ession"
+)
+map("<leader>ss", "<CMD>mksession<CR>", "[s]ave [s]ession")
 
 -- trim whitespace
-local trailspace = require("mini.trailspace")
-map("<leader>tw", trailspace.trim, "[t]rim [w]hitespace")
+map(
+  "<leader>tw",
+  require("mini.trailspace").trim,
+  "[t]rim [w]hitespace"
+)
 
 -- Disable arrow keys in normal mode
-local function show_msg()
-  cmd('echo "retard."')
-  vim.defer_fn(function()
-      cmd('echon ""')
-  end, 3000)
+local keys = { '<left>', '<right>', '<up>', '<down>' }
+for i = 1, #keys do
+  map(keys[i], function() echo("retard.") end)
 end
-map('<left>', show_msg)
-map('<right>', show_msg)
-map('<up>', show_msg)
-map('<down>', show_msg)
 
 -- window navigation
 map('<C-h>', '<C-w><C-h>', 'Move focus to the left window')
@@ -64,10 +76,10 @@ map('<C-j>', '<C-w><C-j>', 'Move focus to the lower window')
 map('<C-k>', '<C-w><C-k>', 'Move focus to the upper window')
 
 -- buffer navigation
-map("<A-,>", ":bp<CR>", "next buffer")
-map("<A-.>", ":bn<CR>", "next buffer")
-map("<A-w>", ":w<CR>:bwipeout<CR>", "save and wipeout buffer")
-map("<A-c>", ":bwipeout!<CR>", "force wipeout buffer")
+map("<A-,>", "<CMD>bp<CR>", "next buffer")
+map("<A-.>", "<CMD>bn<CR>", "next buffer")
+map("<A-w>", "<CMD>w<CR><CMD>bwipeout<CR>", "save and wipeout buffer")
+map("<A-c>", "<CMD>bwipeout!<CR>", "force wipeout buffer")
 
 -- highlight when yanking
 api.nvim_create_autocmd('TextYankPost', {
@@ -84,41 +96,44 @@ api.nvim_create_autocmd("LspAttach", {
   group = api.nvim_create_augroup("lsp-attach", { clear = true }),
 
   callback = function(event)
-    local map_ = function(keys, func, desc)
-      vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    local function lsp_map(keys, func, desc)
+      local opts = {
+        noremap = true, silent = true, buffer = event.buf, desc = "LSP: " .. desc
+      }
+      vim.keymap.set("n", keys, func, opts)
     end
 
     -- Jump to the definition of the word under your cursor.
     --  To jump back, press <C-t>.
-    map_("gd", ts.lsp_definitions, "[g]oto [d]efinition")
+    lsp_map("gd", ts.lsp_definitions, "[g]oto [d]efinition")
 
     -- Find references for the word under your cursor.
-    map_("gr", ts.lsp_references, "[g]oto [r]eferences")
+    lsp_map("gr", ts.lsp_references, "[g]oto [r]eferences")
 
     -- Jump to the implementation of the word under your cursor.
     --  Useful when your language has ways of declaring types without an actual implementation.
-    map_("gi", ts.lsp_implementations, "[g]oto [i]mplementation")
+    lsp_map("gi", ts.lsp_implementations, "[g]oto [i]mplementation")
 
     -- Jump to the type of the word under your cursor.
     --  Useful when you"re not sure what type a variable is and you want to see
     --  the definition of its *type*, not where it was *defined*.
-    map_("gt", ts.lsp_type_definitions, "[g]oto [t]ype definition")
+    lsp_map("gt", ts.lsp_type_definitions, "[g]oto [t]ype definition")
 
     -- Fuzzy find all the symbols in your current document.
     --  Symbols are things like variables, functions, types, etc.
-    map_("<leader>ds", ts.lsp_document_symbols, "[d]ocument [s]ymbols")
+    lsp_map("<leader>ds", ts.lsp_document_symbols, "[d]ocument [s]ymbols")
 
     -- Fuzzy find all the symbols in your current workspace.
     --  Similar to document symbols, except searches over your entire project.
-    map_("<leader>ws", ts.lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
+    lsp_map("<leader>ws", ts.lsp_dynamic_workspace_symbols, "[w]orkspace [s]ymbols")
 
     -- Rename the variable under your cursor.
     --  Most Language Servers support renaming across files, etc.
-    map_("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+    lsp_map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
     -- Execute a code action, usually your cursor needs to be on top of an error
     -- or a suggestion from your LSP for this to activate.
-    map_("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
+    lsp_map("<leader>ca", vim.lsp.buf.code_action, "[c]ode [a]ction")
 
     -- interact with diagnostic messages
     map("[d", vim.diagnostic.goto_prev, "prev [d]iagnostic message")
@@ -127,15 +142,15 @@ api.nvim_create_autocmd("LspAttach", {
     map("<leader>q", vim.diagnostic.setloclist, "show [q]uick fix")
 
     -- Opens a popup that displays documentation about the word under your cursor
-    --  See `:help K` for why this keymap.
-    map_("K", vim.lsp.buf.hover, "[K] Hover Documentation")
+    --  See `<CMD>help K` for why this keymap.
+    lsp_map("K", vim.lsp.buf.hover, "[K] Hover Documentation")
 
-    -- WARN: This is not Goto Definition, this is Goto Declaration.
+    -- WARN<CMD> This is not Goto Definition, this is Goto Declaration.
     --  For example, in C this would take you to the header.
-    map_("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
+    lsp_map("gD", vim.lsp.buf.declaration, "[g]oto [D]eclaration")
 
     -- formatting
-    map_("<leader>fb", vim.lsp.buf.format, "[f]ormat [b]uffer")
+    lsp_map("<leader>fb", vim.lsp.buf.format, "[f]ormat [b]uffer")
 
     -- The following two autocommands are used to highlight references of the
     -- word under your cursor when your cursor rests there for a little while.
@@ -175,20 +190,19 @@ api.nvim_create_autocmd("LspAttach", {
     --
     -- This may be unwanted, since they displace some of your code
     if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
-      map("<leader>th", function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-      end, "[t]oggle inlay [h]ints")
+      map(
+        "<leader>th",
+        function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end,
+        "[t]oggle inlay [h]ints"
+      )
     end
   end,
 })
 
 -- codecompanion
-local ccopts = { noremap = true, silent = true }
-map("<C-a>", "<cmd>CodeCompanionActions<cr>", "Show code companion actions", ccopts, "n")
-map("<C-a>", "<cmd>CodeCompanionActions<cr>", "Show code companion actions", ccopts, "v")
-map("<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", "Toggle code companion chat", ccopts, "n")
-map("<LocalLeader>a", "<cmd>CodeCompanionChat Toggle<cr>", "Toggle code companion chat", ccopts, "v")
-map("ga", "<cmd>CodeCompanionChat Add<cr>", "Add selection to chat", ccopts, "v")
+map("<C-a>", "<CMD>CodeCompanionActions<CR>", "Show code companion actions", {"n", "v"})
+map("<LocalLeader>a", "<CMD>CodeCompanionChat Toggle<CR>", "Toggle code companion chat", {"n", "v"})
+map("ga", "<CMD>CodeCompanionChat Add<CR>", "Add selection to chat", "v")
 
 -- Define groups for which-key
 local wk = require("which-key")
