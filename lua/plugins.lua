3f67c4deefa6c6938787489af91d8f491099984b
Tony Okeke <tonykabilanokeke@gmail.com>
2025-03-17 15:58:38 -0400

    fix: update oil config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,7 +17,10 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      columns = { "icon" },
+      view_options = { show_hidden = true }
+    },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
   },
