e7e9ccb4ef2065a8905d66b4fed600ae10f68700
Tony Okeke <tonykabilanokeke@gmail.com>
2025-03-19 22:18:39 -0400

    feat: add persistence for resizing window

--- lua/config/terminal.lua
+++ lua/config/terminal.lua
@@ -3,56 +3,96 @@ local M = {}
 -- global var to track buffer & window
 vim.g.terminal_buf_id = nil
 vim.g.terminal_win_id = nil
+vim.g.terminal_win_w = nil
+vim.g.terminal_win_h = nil
 
-function M.toggle_terminal()
-  -- if the terminal window exists & is open, close it
+-- define functions to resize the window
+M.resize_terminal = {
+  taller = function()
+    vim.g.terminal_win_h = vim.g.terminal_win_h + 2
+    vim.api.nvim_win_set_height(vim.g.terminal_win_id, vim.g.terminal_win_h)
+  end,
+  shorter = function()
+    vim.g.terminal_win_h = vim.g.terminal_win_h - 2
+    vim.api.nvim_win_set_height(vim.g.terminal_win_id, vim.g.terminal_win_h)
+  end,
+  wider = function()
+    vim.g.terminal_win_w = vim.g.terminal_win_w + 2
+    vim.api.nvim_win_set_width(vim.g.terminal_win_id, vim.g.terminal_win_w)
+  end,
+  narrower = function()
+    vim.g.terminal_win_w = vim.g.terminal_win_w - 2
+    vim.api.nvim_win_set_width(vim.g.terminal_win_id, vim.g.terminal_win_w)
+  end,
+}
+
+function M.toggle_terminal(opts)
+  opts = opts or {}
+  local w = opts.width or 0.6
+  local h = opts.height or 0.85
+
+  local get_win_config = function()
+    return {
+      relative = "editor",
+      width = vim.g.terminal_win_w or math.floor(vim.o.columns * w),
+      height = vim.g.terminal_win_h or math.floor(vim.o.lines * h),
+      col = math.floor((vim.o.columns - (vim.g.terminal_win_w or math.floor(vim.o.columns * w))) / 2),
+      row = math.floor((vim.o.lines - (vim.g.terminal_win_h or math.floor(vim.o.lines * h))) / 2),
+      border = "rounded",
+    }
+  end
+
+  -- terminal window exists + is open -> close
   if vim.g.terminal_win_id and vim.api.nvim_win_is_valid(vim.g.terminal_win_id) then
     vim.api.nvim_win_close(vim.g.terminal_win_id, true)
     vim.g.terminal_win_id = nil
     return
   end
 
-  -- if the buffer exists but the window is closed, reopen it
+  -- buffer exists + window closed -> reopen
   if vim.g.terminal_buf_id and vim.api.nvim_buf_is_valid(vim.g.terminal_buf_id) then
-    local win_config = {
-      relative = "editor",
-      width = math.floor(vim.o.columns * 0.8),                                 -- 80% editor width
-      height = math.floor(vim.o.lines * 0.8),                                  -- 80% editor height
-      col = math.floor((vim.o.columns - math.floor(vim.o.columns * 0.8)) / 2), -- center horizontally
-      row = math.floor((vim.o.lines - math.floor(vim.o.lines * 0.8)) / 2),     -- center horizontally
-      style = "minimal",
-      border = "rounded",
-    }
-    vim.g.terminal_win_id = vim.api.nvim_open_win(vim.g.terminal_buf_id, true, win_config)
+    vim.g.terminal_win_id = vim.api.nvim_open_win(vim.g.terminal_buf_id, true, get_win_config())
     return
   end
 
   -- create a new terminal
   vim.g.terminal_buf_id = vim.api.nvim_create_buf(false, true) -- not listed, scratch buffer
-  local win_config = {
-    relative = "editor",
-    width = math.floor(vim.o.columns * 0.8),                                 -- 80% editor width
-    height = math.floor(vim.o.lines * 0.8),                                  -- 80% editor height
-    col = math.floor((vim.o.columns - math.floor(vim.o.columns * 0.8)) / 2), -- center horizontally
-    row = math.floor((vim.o.lines - math.floor(vim.o.lines * 0.8)) / 2),     -- center horizontally
-    -- style = "minimal",
-    border = "rounded",
-  }
-  vim.g.terminal_win_id = vim.api.nvim_open_win(vim.g.terminal_buf_id, true, win_config)
+  vim.g.terminal_win_id = vim.api.nvim_open_win(vim.g.terminal_buf_id, true, get_win_config())
+  vim.g.terminal_win_h = get_win_config().height
+  vim.g.terminal_win_w = get_win_config().width
+
   vim.fn.termopen(vim.o.shell, {
     on_exit = function()
       if vim.g.terminal_win_id and vim.api.nvim_win_is_valid(vim.g.terminal_win_id) then
         vim.api.nvim_win_close(vim.g.terminal_win_id, true)
         vim.g.terminal_win_id = nil
         vim.g.terminal_buf_id = nil
+        vim.g.terminal_win_h = nil
+        vim.g.terminal_win_w = nil
       end
     end
   })
-  vim.cmd("startinsert") -- enter terminal mode immediately
 
-  -- buffer-local keymap to exit terminal mode
-  vim.api.nvim_buf_set_keymap(vim.g.terminal_buf_id, "t", "<C-q>", "<C-\\><C-n>",
-    { noremap = true, silent = true, desc = "Exit terminal mode" })
+  vim.api.nvim_buf_set_keymap(
+    vim.g.terminal_buf_id, "n", "<C-A-t>",
+    "<CMD>lua require('config.terminal').resize_terminal.taller()<CR>",
+    { silent = true, desc = "resize [t]aller" }
+  )
+  vim.api.nvim_buf_set_keymap(
+    vim.g.terminal_buf_id, "n", "<C-A-s>",
+    "<CMD>lua require('config.terminal').resize_terminal.shorter()<CR>",
+    { silent = true, desc = "resize [s]horter" }
+  )
+  vim.api.nvim_buf_set_keymap(
+    vim.g.terminal_buf_id, "n", "<C-A-w>",
+    "<CMD>lua require('config.terminal').resize_terminal.wider()<CR>",
+    { silent = true, desc = "resize [w]ider" }
+  )
+  vim.api.nvim_buf_set_keymap(
+    vim.g.terminal_buf_id, "n", "<C-A-n>",
+    "<CMD>lua require('config.terminal').resize_terminal.narrower()<CR>",
+    { silent = true, desc = "resize [n]arrower" }
+  )
 end
 
 return M
