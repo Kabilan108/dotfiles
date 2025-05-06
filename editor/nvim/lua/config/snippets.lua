6c0e24a6614c5d03539e8f89e14b4699a464a8c7
Tony Okeke <tonykabilanokeke@gmail.com>
2025-05-06 15:48:46 -0400

    feat: consolidate snippets

--- editor/nvim/lua/config/snippets.lua
+++ editor/nvim/lua/config/snippets.lua
@@ -1,16 +1,28 @@
 local M = {}
 
-function M.setup()
-  local luasnip = require("luasnip")
-  local types = require("luasnip.util.types")
+local luasnip = require "luasnip"
+local types = require "luasnip.util.types"
+local events = require "luasnip.util.events"
 
+local s = luasnip.snippet
+local t = luasnip.text_node
+local i = luasnip.insert_node
+local f = luasnip.function_node
+
+local function load_snippets(snippet_table)
+  for ft, snippets in pairs(snippet_table) do
+    luasnip.add_snippets(ft, snippets)
+  end
+end
+
+function M.setup()
   -- Load friendly-snippets
   require("luasnip.loaders.from_vscode").lazy_load()
   require("luasnip.loaders.from_snipmate").lazy_load()
 
   -- Configure LuaSnip
   luasnip.config.set_config({
-    history = true, -- keep around last snippet local to jump back
+    history = true,                            -- keep around last snippet local to jump back
     updateevents = "TextChanged,TextChangedI", -- update changes as you type
     enable_autosnippets = true,
     ext_opts = {
@@ -22,9 +34,7 @@ function M.setup()
     },
   })
 
-  -- Load custom snippets
-  luasnip.add_snippets("python", require("snippets.python"))
-  luasnip.add_snippets("go", require("snippets.go"))
+  load_snippets(M.snippets)
 
   -- Key mappings
   vim.keymap.set({ "i", "s" }, "<C-k>", function()
@@ -45,5 +55,46 @@ function M.setup()
     end
   end, { silent = true, desc = "LuaSnip next choice" })
 end
+
+M.snippets = {}
+
+M.snippets.all = {
+  s("shell_script", {
+    t({ "#!/usr/bin/env bash", "# vim: syn=bash ft=bash", "" }),
+  })
+}
+
+M.snippets.go = {
+  s("gofunc", {
+    t("go func() {"),
+    t({ "", "\t" }),
+    i(0),
+    t({ "", "}()" }),
+  }),
+}
+
+M.snippets.python = {
+  s("script", {
+    t({
+      "# /// script",
+      "# requires-python = \">=3.11\"",
+      "# dependencies = [\"ipdb\""
+    }),
+    i(1, ""),
+    t({ "]",
+      "# ///",
+      "",
+      "" }),
+    i(0),
+  }),
+  s("ifmain", {
+    t({ "if __name__ == \"__main__\":",
+      "    " }),
+    i(1, "pass"),
+  }),
+  s({ trig = "set_trace" }, {
+    t("import ipdb; ipdb.set_trace()"),
+  }),
+}
 
 return M
