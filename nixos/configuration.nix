9c121fd7215ff8961f247cc8d99547a49c2bf4e7
Tony Okeke <tonykabilanokeke@gmail.com>
2025-05-11 12:33:53 -0400

    feat: add nixos configuration

---
+++ nixos/configuration.nix
@@ -0,0 +1,227 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl."net.ipv4.ip_unprivileged_port_start" = 80;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.hostName = "sietch";
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # docker
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kabilan = {
+    isNormalUser = true;
+    description = "Tony Kabilan Okeke";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    packages = with pkgs; [];
+    shell = pkgs.bashInteractive;
+    openssh.authorizedKeys.keyFiles = [
+      /etc/nixos/ssh_authorized_keys
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # link /libexec from dreivations to /run/current-system/sw
+  environment.pathsToLink = [ "/libexec" ];
+
+  # set up X11 and i3wm
+  services.displayManager.defaultSession = "none+i3";
+  services.xserver = {
+    enable = true;
+
+    desktopManager = {
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      extraPackages = with pkgs; [
+        autotiling
+        feh
+        dunst
+        light
+        rofi
+        picom
+        polybar
+        betterlockscreen
+        flameshot
+        playerctl
+        pulseaudio
+        autorandr
+        arandr
+        xorg.xrandr
+        xorg.xev
+      ];
+    };
+
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
+
+  # fonts
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraMono" ]; })
+    fira-mono
+  ];
+
+  # configure nvidia drivers
+  ## load nvidia driver for Xorg & Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+  ## enable opengl
+  hardware.graphics = {
+    enable = true;
+  };
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    brave
+    baobab
+    discord
+    gparted
+    inkscape
+    libreoffice-fresh
+    nautilus
+    obsidian
+    pavucontrol
+    slack
+    spotify
+    telegram-desktop
+    zotero
+
+    bashInteractive
+    cmake
+    fuse
+    ffmpeg_6-full
+    git
+    gnumake
+    jq
+    imagemagick
+    openssl
+    psmisc
+    tailscale
+    tmux
+    wget
+    xclip
+
+    htop
+    nvtopPackages.full
+
+    direnv
+    delta
+    fd
+    fzf
+    ghostty
+    neofetch
+    ripgrep
+    sd
+
+    bun
+    cargo
+    clang
+    clang-tools
+    go
+    lua
+    luajitPackages.luarocks
+    luajitPackages.magick
+    nodejs_20
+    zig
+  ] ++ (
+    (import ./go-tools.nix) { inherit pkgs lib; }
+  );
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+
+  # List services that you want to enable:
+  services.gnome.gnome-keyring.enable = true;
+
+  # Enable tailscale
+  services.tailscale.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.KbdInteractiveAuthentication = false;
+  };
+
+  # Open ports in the firewall.
+  networking.firewall = {
+    enable = true;
+    interfaces."tailscale0" = {
+      allowedTCPPorts = [ 22 80 443 ];
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
---
+++ nixos/go-tools.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib }:
+
+[
+  (pkgs.buildGoModule rec {
+    pname = "dump";
+    version = "0.2.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "kabilan108";
+      repo = "dump";
+      rev = "v${version}";
+      sha256 =  "sha256-rAZofwBMgcVgH5kdwkQ1IQqCAiQTZjcv2+SaU/xPPKE=";
+    };
+
+    vendorHash = "sha256-A8PH2ITmJE8SD9KVTN76OyXZrmc/oq9JH8Vm0HFZWPw=";
+
+    meta = with pkgs.lib; {
+      description = "A CLI tool to dump stuff (replace with actual description)";
+      homepage = "https://github.com/kabilan108/dump";
+      license = licenses.mit;
+      maintainers = [ "kabilan108" ];
+    };
+  })
+
+  (pkgs.buildGoModule rec {
+    pname = "diffgpt";
+    version = "0.4.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "kabilan108";
+      repo = "diffgpt";
+      rev = "v${version}";
+      sha256 =  "sha256-ekbT5W3mB8ra4MPJlbdUQdWMEl8i7cHkwoN84P78AwY=";
+    };
+
+    vendorHash = "sha256-YMPiHe2DEA/1E8wtB1GJf/pvJ0vl3TjfquZdvDA9NDU=";
+
+    meta = with pkgs.lib; {
+      description = "write commit messages with llms";
+      homepage = "https://github.com/kabilan108/diffgpt";
+      license = licenses.mit;
+      maintainers = [ "kabilan108" ];
+    };
+  })
+]
