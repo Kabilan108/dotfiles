c41729d6f91d07255e4d4e6ca1d2ccecd4b36a35
Tony Okeke <tonykabilanokeke@gmail.com>
2025-05-17 14:28:38 -0400

    feat: set permissions for /vault and /library mounts

--- nixos/machine-configs/sietch-hardware-config.nix
+++ nixos/machine-configs/sietch-hardware-config.nix
@@ -36,6 +36,11 @@
     [ { device = "/dev/disk/by-label/SWAP"; }
     ];
 
+  systemd.tmpfiles.rules = [
+    "d /vault 0755 kabilan users - -"
+    "d /library 0755 kabilan users - -"
+  ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
