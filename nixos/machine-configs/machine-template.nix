d69cdf041e12468c4114aad528d24b0fb470f201
Tony Okeke <tonykabilanokeke@gmail.com>
2025-05-18 10:32:27 -0400

    feat: add env var defaults

--- nixos/machine-configs/machine-template.nix
+++ nixos/machine-configs/machine-template.nix
@@ -3,5 +3,35 @@
   enableNvidia = false;
   env = rec {
     WALLPAPER = "";
+
+    # opts for 3rd party tools
+    FZF_DEFAULT_OPTS = "--reverse";
+    UV_LINK_MODE = "copy";
+    UV_SYSTEM_PYTHON = 1;
+
+    # my tools
+    DIFFGPT_API_KEY = "";
+    DIFFGPT_BASE_URL = "https://openrouter.ai/api/v1/";
+    DIFFGPT_MODEL = "openai/gpt-4o-mini";
+    PYREPL_PORT = 5678;
+    SIREN_API_KEY = "";
+
+    # data dirs
+    USER_DATA = "/vault/userdata";
+    TORCH_HOME = "${USER_DATA}/torch";
+    FASTAI_HOME = "${USER_DATA}/fastai";
+    HF_HOME = "${USER_DATA}/huggingface";
+    HF_DATASETS_CACHE = "$HF_HOME/datasets";
+    LLM_USER_PATH = "${USER_DATA}/datasette-llm";
+    OLLAMA_MODELS = "${USER_DATA}/ollama/models";
+
+    # llms
+    ANTHROPIC_API_KEY = "";
+    GEMINI_API_KEY = "";
+    GROQ_API_KEY = "";
+    OPENAI_API_KEY = "";
+    OPENROUTER_API_KEY = "";
+    PERPLEXITY_API_KEY = "";
+    DEEPSEEK_API_KEY = "";
   };
 }
