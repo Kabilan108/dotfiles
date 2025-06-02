a7d15567712e5430411bf8eba34f2054f7ad7cb6
Tony Okeke <tonykabilanokeke@gmail.com>
2025-06-01 22:10:16 -0400

    feat: implement multi-turn conversation support

--- conf/nvim/lua/config/llm.lua
+++ conf/nvim/lua/config/llm.lua
@@ -19,4 +19,5 @@ droid.setup({
 utils.map("<leader>ll", { "n", "v" }, droid.help_completion, "llm: help")
 utils.map("<leader>le", { "n", "v" }, droid.edit_completion, "llm: edit")
 utils.map("<leader>lm", { "n", "v" }, droid.select_model, "llm: select model")
+utils.map("<leader>ln", { "n", "v" }, droid.jump_to_new, "llm: jump to new")
 utils.map("<leader>lc", "n", droid.cancel_completion, "llm: cancel stream")
