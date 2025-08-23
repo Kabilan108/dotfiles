996e008bb13002826aaf9e7dbfac7a083cb80e24
Tony Okeke <tonykabilanokeke@gmail.com>
2025-08-23 13:25:40 -0400

    fix: install plugins with llm

--- dev/systemd-services.nix
+++ dev/systemd-services.nix
@@ -75,7 +75,7 @@ in
       Type = "oneshot";
       ExecStart = ''
         ${pkgs.uv}/bin/uv tool install -U git+https://github.com/karpathy/rendergit
-        ${pkgs.uv}/bin/uv tool install -U llm
+        ${pkgs.uv}/bin/uv tool install -U --with llm-cmd --with llm-anthropic llm
         ${pkgs.uv}/bin/uv tool install -U shell_sage
       '';
     };
