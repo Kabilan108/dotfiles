210dd30b98e1aa55a21fd03efaba2ab916f0f8ea
Tony Okeke <tonykabilanokeke@gmail.com>
2025-10-27 10:25:08 -0400

    build: update dependencies

--- packages/cursor/default.nix
+++ packages/cursor/default.nix
@@ -9,7 +9,7 @@ let
   version = "1.7.17";
   src = fetchurl {
     url = "https://api2.cursor.sh/updates/download/golden/linux-x64/cursor/1.7";
-    hash = "sha256-nhDDdXE5/m9uASiQUJ4GHfApkzkf9ju5b8s0h6BhpjQ=";
+    hash = "sha256-BKxFrfKFMWmJhed+lB5MjYHbCR9qZM3yRcs7zWClYJE=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
