51e26c9c438d3a58061c14a2913ca67877a58dcb
Tony Okeke <tonykabilanokeke@gmail.com>
2025-11-08 16:19:34 -0500

    update flake

--- packages/cursor/default.nix
+++ packages/cursor/default.nix
@@ -9,7 +9,7 @@ let
   version = "1.7.17";
   src = fetchurl {
     url = "https://api2.cursor.sh/updates/download/golden/linux-x64/cursor/2.0";
-    hash = "sha256-ndss1uOAHk04Y6KnPWGqm+dTyGCrVOR1uJW/8nD/T/s=";
+    hash = "sha256-zT9GhdwGDWZJQl+WpV2txbmp3/tJRtL6ds1UZQoKNzA=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
