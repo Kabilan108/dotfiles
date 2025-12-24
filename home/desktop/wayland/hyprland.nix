0eb28a0902024e893ccc55570d2067a204e5e2ad
Tony Okeke <tonykabilanokeke@gmail.com>
2025-12-24 13:29:38 -0500

    update workpace config

--- home/desktop/wayland/hyprland.nix
+++ home/desktop/wayland/hyprland.nix
@@ -168,7 +168,7 @@ in
         "workspace 9 silent, class:^(spotify)$"
         "workspace 2 silent, class:^(obsidian)$"
         "workspace 2 silent, class:^(Slack)$"
-        "workspace 3 silent, class:^(Chromium-browser)$"
+        "workspace 1 silent, class:^(Chromium-browser)$"
         "workspace 1 silent, class:^(zen-beta)$"
 
         # picture-in-picture
