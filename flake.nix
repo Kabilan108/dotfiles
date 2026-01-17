2c57dbe7d1a8514ff98c6f31fc45e57549149256
Tony Okeke <tonykabilanokeke@gmail.com>
2026-01-17 14:09:43 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -130,7 +130,7 @@
     diffgpt.url = "github:kabilan108/diffgpt";
     dump.url = "github:kabilan108/dump";
     rollouts.url = "github:kabilan108/rollouts";
-    try.url = "github:tobi/try";
+    try.url = "github:tobi/try/55baadc3f90ee7b7cbfa3d6c7b2c29db22151d5d";
     worktrunk.url = "github:kabilan108/worktrunk";
   };
 }
