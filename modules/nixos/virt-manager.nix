19852b9c8364ebeacf7efeeb9b28af723234637d
Tony Okeke <tonykabilanokeke@gmail.com>
2026-01-19 14:09:44 -0500

    enable virtiofsd

--- modules/nixos/virt-manager.nix
+++ modules/nixos/virt-manager.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.virt-manager.enable = true;
   users.groups.libvirtd.members = [ "kabilan" ];
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.vhostUserPackages = [ pkgs.virtiofsd ];
+  };
   virtualisation.spiceUSBRedirection.enable = true;
 }
