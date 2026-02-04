c85f95879e36a025e96f6b4a9731c7a93cf30d1f
Tony Okeke <tonykabilanokeke@gmail.com>
2026-02-04 00:05:54 -0500

    niri: prefer cache and skip checks

--- modules/nixos/wayland/niri.nix
+++ modules/nixos/wayland/niri.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }:
 {
-  programs.niri.enable = true;
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  niriPackage = inputs."niri-flake".packages.${pkgs.system}.niri-stable.overrideAttrs (_: {
+    doCheck = false;
+  });
+in
+{
+  niri-flake.cache.enable = true;
+
+  programs.niri = {
+    enable = true;
+    package = niriPackage;
+  };
 
   security.pam.services.swaylock = { };
 
