276f8830ffef588f28fd5c042e654d10aa2ab936
Tony Okeke <tonykabilanokeke@gmail.com>
2026-02-17 11:01:12 -0500

    enfore a maximum mic volume of 30% on jacurutu

--- machines/jacurutu/default.nix
+++ machines/jacurutu/default.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }:
+let
+  micEnforce = pkgs.writeShellScript "mic-volume-enforce" ''
+    SOURCE="alsa_input.pci-0000_c1_00.6.analog-stereo"
+    MAX=35
+
+    ${pkgs.pulseaudio}/bin/pactl subscribe | while read -r line; do
+      if [[ "$line" == *"'change' on source"* ]]; then
+        vol=$(${pkgs.pulseaudio}/bin/pactl get-source-volume "$SOURCE" 2>/dev/null \
+          | ${pkgs.gnugrep}/bin/grep -oP '\d+(?=%)' | head -1)
+        if [[ -n "$vol" && "$vol" -gt "$MAX" ]]; then
+          ${pkgs.pulseaudio}/bin/pactl set-source-volume "$SOURCE" 30%
+        fi
+      fi
+    done
+  '';
+in
 {
   imports = [
     ./framework.nix
@@ -6,6 +22,12 @@
   ];
 
   networking.hostName = "jacurutu";
+
+  services.pipewire.wireplumber.extraConfig."50-mic-volume" = {
+    "wireplumber.settings" = {
+      "device.routes.default-source-volume" = 0.30;
+    };
+  };
 
   environment = {
     systemPackages = with pkgs; [ fprintd ];
@@ -15,5 +37,19 @@
     programs.niri.config = null;
     wallpaper.desktop = "$HOME/dotfiles/wallpapers/witcher.png";
     wallpaper.lockscreen = "$HOME/dotfiles/wallpapers/witcher.png";
+
+    systemd.user.services.mic-volume-enforce = {
+      Unit = {
+        Description = "Enforce internal mic volume cap";
+        After = [ "pipewire.service" "wireplumber.service" ];
+        Requires = [ "pipewire.service" ];
+      };
+      Service = {
+        ExecStart = "${micEnforce}";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+      Install.WantedBy = [ "default.target" ];
+    };
   };
 }
