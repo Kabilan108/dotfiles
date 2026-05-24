ce1d8026d9ec61791ef5eb62d56ac47bb63fb66c
Tony Okeke <tonykabilanokeke@gmail.com>
2026-05-24 14:04:56 -0400

    fix: wrap in LD_LIBRARY_PATH to get GPU stats on sietch

--- modules/home/btop.nix
+++ modules/home/btop.nix
@@ -1,9 +1,21 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
+let
+  btopWithRuntimeGpuLibraries = pkgs.symlinkJoin {
+    name = "btop-with-runtime-gpu-libraries";
+    paths = [ pkgs.btop ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/btop \
+        --prefix LD_LIBRARY_PATH : /run/opengl-driver/lib
+    '';
+  };
+in
 {
   stylix.targets.btop.enable = true;
 
   programs.btop = {
     enable = true;
+    package = btopWithRuntimeGpuLibraries;
     settings = {
       theme_background = false;
     };
