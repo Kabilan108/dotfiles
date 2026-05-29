365e5e379d35a8d2c3be523a401a62d3be0385fd
Tony Okeke <tonykabilanokeke@gmail.com>
2026-05-29 12:10:12 -0400

    fix: update lualine catppuccin theme
    - Use the Catppuccin v2 lualine theme name for Mocha
    
    - Avoid lualine falling back to auto due to missing plain catppuccin theme

--- config/nvim/lua/config/lualine.lua
+++ config/nvim/lua/config/lualine.lua
@@ -23,7 +23,7 @@ local config = {
     -- Disable sections and component separators
     component_separators = '',
     section_separators = '',
-    theme = 'catppuccin',
+    theme = 'catppuccin-mocha',
   },
   sections = {
     -- these are to remove the defaults
