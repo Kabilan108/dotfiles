22b61c06c1ce5a62f4ed84c04dbca7bfd37b1e23
Tony Okeke <tonykabilanokeke@gmail.com>
2026-07-23 22:43:23 -0400

    run nix gc once every week

--- configuration.nix
+++ configuration.nix
@@ -37,7 +37,7 @@
       gc = {
         automatic = true;
         dates = "weekly";
-        options = "--delete-older-than 14d";
+        options = "--delete-older-than 7d";
       };
       extraOptions = "trusted-users = root kabilan";
       settings = {
