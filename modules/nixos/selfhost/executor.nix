0415b5bb76adfebc83fd39245e5ea93fd82781a8
Tony Okeke <tonykabilanokeke@gmail.com>
2026-08-07 22:54:59 -0400

    update executor to 1.5.37

--- modules/nixos/selfhost/executor.nix
+++ modules/nixos/selfhost/executor.nix
@@ -6,11 +6,12 @@
 
   virtualisation.oci-containers.containers.executor = {
     # digest-pinned; bump with bin/image-pins or the update-image-pins skill
-    image = "ghcr.io/rhyssullivan/executor-selfhost:latest@sha256:aa985b446aafd28a3b723559ec62b7dbc356bb27eb433d87e82b213dd55f9b64";
+    image = "ghcr.io/rhyssullivan/executor-selfhost:latest@sha256:125123681a14e44d679f22d259ce178bf605886e54c10b5b08b09b19c09f4695";
     ports = [ "127.0.0.1:8302:4788" ];
     volumes = [ "/var/lib/executor:/data" ];
     environment = {
       EXECUTOR_WEB_BASE_URL = "https://executor.sole-pierce.ts.net";
+      EXECUTOR_DISABLE_ANALYTICS = "1";
       # sandboxed code must not reach loopback: vaultwarden/jellyfin/siren live there
       EXECUTOR_ALLOW_LOCAL_NETWORK = "false";
     };
