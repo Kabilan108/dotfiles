b4fb0271c5580d10a0a726e3ee8abb3c27b2dfee
Tony Okeke <tonykabilanokeke@gmail.com>
2026-09-01 18:06:14 -0400

    fix import (broken during rebase)

--- home/default.nix
+++ home/default.nix
@@ -53,6 +53,7 @@ in
     ../modules/home/gtk.nix
     ../modules/home/helium.nix
     ../modules/home/pwas.nix
+    ../modules/home/stillsuit
     ../modules/home/stylix.nix
     ../modules/home/wallpaper.nix
     ../modules/home/zen
