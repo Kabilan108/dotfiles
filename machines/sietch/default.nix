fab51089d1c1a61a4b44ec4e474b45ca9eacc6f3
Tony Okeke <tonykabilanokeke@gmail.com>
2026-09-08 01:41:42 -0400

    disable codex remote -> managed by jacurutu codex desktop running via ssh

--- machines/sietch/default.nix
+++ machines/sietch/default.nix
@@ -43,7 +43,7 @@
 
   home-manager.users.kabilan = {
     dotfiles.services = {
-      codex-remote-control.enable = true;
+      codex-remote-control.enable = false;
       moberg.devMaintenance.enable = true;
       t3-code.enable = true;
       wayvnc.enable = true;
